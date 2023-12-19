be2f46907076d1e0ded03d5f9c579b2c5f063abc
Vadim <korovin555@.gmail.com>
2023-12-19 20:41:19 +0700

    Начал

--- tests/level_2/test_five_replace_word.py
+++ tests/level_2/test_five_replace_word.py
@@ -2,11 +2,11 @@ from functions.level_2.five_replace_word import replace_word
 import pytest
 
 @pytest.mark.parametrize(
-    'verb_male, verb_female, gender, expected',
+    'text, verb_female, gender, expected',
     [
         ('v_male', 'v_female', 'male', 'v_male'),
         ('v_male', 'v_female', 'female', 'v_female'),       
     ]
 )
 def test_genderalize(verb_male, verb_female, gender, expected):
-    assert genderalize(verb_male, verb_female, gender) == expected
+    assert replace_word(verb_male, verb_female, gender) == expected
